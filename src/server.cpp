7f60707c0fe4c5a7f5abf887ad238ba503ed985c
Jim DeVona <jim.devona@gmail.com>
2013-09-22 14:36:55 -0400

    Use xdg-open to edit config file. http://portland.freedesktop.org/xdg-utils-1.0/xdg-open.html #17

--- src/server.cpp
+++ src/server.cpp
@@ -34,7 +34,7 @@
 #define TOOLBAR_LABEL_DEFAULT "Mobile Mouse Linux Server"
 #define TOOLBAR_ICON_DEFAULT "/usr/share/mmserver/icons/mm-idle.svg"
 #define TOOLBAR_ICON_CONNECTED "/usr/share/mmserver/icons/mm-connected.svg"
-#define PREFERENCES_EDITOR "gnome-open"
+#define PREFERENCES_EDITOR "xdg-open"
 #endif
 #define DEFAULT_CONFIG "/usr/share/mmserver/mmserver.conf"
 #define USER_CONFIG_DIR ".mmserver"
