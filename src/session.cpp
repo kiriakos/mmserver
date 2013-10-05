551932bf118e82c693b69dedb6dc48c3a7c24e64
Jim DeVona <jim.devona@gmail.com>
2013-10-04 22:32:31 -0400

    Disable clipboardsync text test

--- src/session.cpp
+++ src/session.cpp
@@ -205,7 +205,7 @@ void* MobileMouseSession(void* context)
 		{
 			if (option == "CLIPBOARDSYNC") {
 				syslog(LOG_INFO, "Clipboard sync: %s", optval.c_str());
-				if (optval == "1") {
+				/*if (optval == "1") {
 					
 					char m[1024];
 					snprintf(m, sizeof(m), "CLIPBOARDUPDATE\x1e" "TEXT\x1f" "Hello, world!\x04");
@@ -215,7 +215,7 @@ void* MobileMouseSession(void* context)
 						close(client);
 						break;
 					}
-				}
+				}*/
 			}
 			else if (option == "PRESENTATION") {
 				/* Presumably concerns the extra in-app purchase "pro presentation module" */
