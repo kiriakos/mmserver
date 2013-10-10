85fe17f832eddb67e05759aaed7da386f9b7d633
Jim DeVona <jim.devona@gmail.com>
2013-10-10 14:03:50 -0400

    Expanded comment notes on clipboard getter

--- src/xwrapper.cpp
+++ src/xwrapper.cpp
@@ -164,6 +164,8 @@ XClipboardInterface::XClipboardInterface(const std::string display) {
 	if ((m_display = XOpenDisplay(display.empty()?NULL:display.c_str())) == NULL) {
 		throw std::runtime_error("cannot open xdisplay");
 	}
+	
+	/* we need a window in order to access the clipboard. not displayed. */
 	m_window = XCreateSimpleWindow(m_display, DefaultRootWindow(m_display), 0, 0, 1, 1, 0, 0, 0);
 }
 
@@ -174,23 +176,46 @@ XClipboardInterface::~XClipboardInterface() {
 
 bool XClipboardInterface::Update(void) {
 	
-    unsigned char *sel_buf;     /* buffer for selection data */
-    unsigned long sel_len = 0;  /* length of sel_buf */
-    XEvent evt;                 /* X Event Structures */
-    unsigned int context = XCLIB_XCOUT_NONE;
-	Atom sseln = XA_CLIPBOARD(m_display);	/* X selection to work with */
+	/* buffer for selection data */
+	unsigned char *sel_buf;
+	
+	/* length of sel_buf */
+	unsigned long sel_len = 0;
+	
+	/* X Event Structures */
+	XEvent evt;
+	
+	/* reading clipboard can be a multi-step process; context tracks current step */
+	unsigned int context = XCLIB_XCOUT_NONE;
+	
+	/* X selection to work with - use XA_PRIMARY for current selected text */
+	Atom sseln = XA_CLIPBOARD(m_display);
+	
+	/* desired format (xcout actually tries to get as UTF-8 first) */
 	Atom target = XA_STRING;
+	
+	/* whether this update has actually changed the cached clipboard content */
 	bool updated = false;
-
+	
 	while (1) {
+		
+		/* initial context is NONE, so this is skipped... */
+		/* second time through, context is SENT, so we request next event... */
 		/* only get an event if xcout() is doing something */
 		if (context != XCLIB_XCOUT_NONE) {
 			XNextEvent(m_display, &evt);
 		}
 		
+		/* with initial context NONE, xcout sends selection request to owner,
+		 * changes context to SENT, and returns... */
+		/* xcout could do a few things the second time through;
+		 * first, it'll see if it can get selection as UTF8; if not, returns fallback, and reverts to string below to try again */
+		/* alternatively, getting the result may require repeated attempts (INCRemental mode) */
+		/* or, it might just get it, setting context back to NONE so we break out */
 		/* fetch the selection, or part of it */
 		xcout(m_display, m_window, evt, sseln, target, &sel_buf, &sel_len, &context);
 		
+		/* initially, context is SENT now */
 		/* fallback is needed. set XA_STRING to target and restart the loop. */
 		if (context == XCLIB_XCOUT_FALLBACK) {
 			context = XCLIB_XCOUT_NONE;
@@ -203,9 +228,11 @@ bool XClipboardInterface::Update(void) {
 			break;
 		}
 	}
-
-    if (sel_len) {
+	
+	if (sel_len) {
 		
+		/* sel_buf is current clipboard content; if different from cached
+		 * content, update the cache and set the updated flag to true. */
 		if (clipcache.compare(0, std::string::npos, (char *)sel_buf, sel_len) != 0) {
 			clipcache.assign((char *)sel_buf, (int)sel_len);
 			updated = true;
@@ -220,6 +247,12 @@ bool XClipboardInterface::Update(void) {
     return updated;
 }
 
+/* return the cached clipboard content as a std::string */
+const std::string XClipboardInterface::GetString() {
+	return clipcache;
+}
+
+/* return the cached clipboard content as a null-terminated C string */
 const char *XClipboardInterface::GetCStr(void) {
 	return clipcache.c_str();
 }
--- src/xwrapper.hpp
+++ src/xwrapper.hpp
@@ -69,6 +69,7 @@ class XClipboardInterface
 		~XClipboardInterface();
 
 		bool Update(void);
+		const std::string GetString(void);
 		const char *GetCStr(void);
 	
 	private:
