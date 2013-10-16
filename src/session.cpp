798fe59b298ef0ea9689d884759918f090f09ce5
Jim DeVona <jim.devona@gmail.com>
2013-10-16 11:32:25 -0400

    Revert "Removed unused keystring handler. (#24)" Keystring packets do appear to be sent in at least one case: when typing with the "shift lock" toggle button enabled (part of the MM toolbar, not the standard iOS keyboard shift key). A bit unclear when sent. This reverts commit d20864c3d18c89d8a8a5049da4ded09cca5119d3.

--- src/session.cpp
+++ src/session.cpp
@@ -553,7 +553,21 @@ void* MobileMouseSession(void* context)
 			}
 			continue;
 		}
-		
+
+		/* handle keystrings... eg. ".com" */
+		std::string keystring;
+		if (pcrecpp::RE("KEYSTRING\x1e(.*?)\x04").FullMatch(packet, &keystring))
+		{
+			std::list<int> keys;
+			for (std::string::const_iterator i = keystring.begin();
+					i != keystring.end(); i++)
+			{
+				keys.push_back(*i);
+			}
+			keyBoard.SendKey(keys);
+			continue;
+		}
+
 		/* screens */
 		std::string mode;
 		if (pcrecpp::RE("SWITCHMODE\x1e(.*?)\x04").FullMatch(packet, &mode))
